d3bd51a2c38965035c7231e471071e7ff4227300
Lahiru Weliwitiya <lwel0005@student.monash.edu>
2023-09-24 22:59:46 +0800

    init commit

---
+++ data_viz.js
@@ -0,0 +1,18 @@
+
+// var vg_1 = "data_bar_chart.vg.json";
+// var vg_1 = "data_stacked_bar_chart.vg.json";
+
+
+vegaEmbed("#viz", vg_1)
+
+.then(function (result) {
+
+    // Access the Vega view instance 
+    // (https://vega.github.io/vega/docs/api/view/) as result.view
+
+    // console.log(result);
+
+})
+.catch(console.error);
+
+
