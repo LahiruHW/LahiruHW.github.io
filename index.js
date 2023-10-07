62f189c99af89436d51fc738f3400e4c62372814
Lahiru Weliwitiya <lwel0005@student.monash.edu>
2023-10-08 05:59:28 +0800

    refactored separate submissions into separate pages for accessibility

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+onWeeek9BtnClick = () => {
+    window.location.href = "../Week 9 Submission/week_9.html";
+}
+
+onWeeek10BtnClick = () => {
+    window.location.href = "../Week 10 Submission/week_10.html";
+}
