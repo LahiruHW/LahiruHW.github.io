a98d592f24b65a0ca7b3e093f8af7db3664bb585
Lahiru Weliwitiya <lwel0005@student.monash.edu>
2023-10-08 18:59:25 +0800

    week 10 files init commit

---
+++ Week 10 Submission/data_viz.js
@@ -0,0 +1,14 @@
+
+var vg_1 = "data_viz.vg.json";
+
+
+
+vegaEmbed("#viz", vg_1)
+
+.then(function (result) {
+
+    // Access the Vega view instance 
+    // (https://vega.github.io/vega/docs/api/view/) as result.view
+
+})
+.catch(console.error);
