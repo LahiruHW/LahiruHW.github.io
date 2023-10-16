a1ddd594ada467d62033dd10e48900debf5ddf09
Lahiru Weliwitiya <lwel0005@student.monash.edu>
2023-10-16 14:11:10 +0800

    final push (I just can't)

--- A2/data_viz.js
+++ A2/data_viz.js
@@ -1,6 +1,7 @@
 
 var vg_1 = "data_viz1.vg.json";
 var vg_2 = "data_viz2.vg.json";
+var vg_3 = "data_viz3.vg.json";
 
 
 vegaEmbed("#viz1", vg_1).then(function (result) {
@@ -27,7 +28,19 @@ console.log( result.view.data('source_1') );
 vegaEmbed("#viz2", vg_2).then(function (result) {
     // Access the Vega view instance 
     // (https://vega.github.io/vega/docs/api/view/) as result.view
-    document.getElementById("viz2-text").innerHTML += '<div id="viz2-text"> This shows that there is a proportional relationship between the GDP per capita and the Happiness level of a country (i.e. indicated by the Happiness Class that the country belongs to)  </div>'
+    document.getElementById("viz2-text").innerHTML += '<div id="viz2-text"> This shows that there is no particular relationship between the known (i.e. average) Generosity of a country and its Happiness level (i.e. indicated by the Happiness Class that the country belongs to)  </div>'
 })
 .catch(console.error);
+
+
+vegaEmbed("#viz3", vg_3).then(function (result) {
+    // Access the Vega view instance 
+    // (https://vega.github.io/vega/docs/api/view/) as result.view
+
+
+
+
+
+
+});
 
